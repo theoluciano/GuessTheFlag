23a9851fbfb0fee5e8496425dac9ffad06ca4f02
Theo Luciano <62947477+theoluciano@users.noreply.github.com>
2024-10-14 16:14:52 -0400

    Initial Commit

---
+++ GuessTheFlagTests/GuessTheFlagTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagTests.swift
+//  GuessTheFlagTests
+//
+//  Created by Theo Luciano on 10/14/24.
+//
+
+import Testing
+@testable import GuessTheFlag
+
+struct GuessTheFlagTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
